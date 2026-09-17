c8a70e9413b88d657aa91f706f66595c71e0084f
g-nzaan <asgmshift@gmail.com>
2026-09-16 18:04:59 -0600

    Cambios en la importaciones de librerías

--- CasoPractico-G5/src/main/java/org/ni/edu/uam/casopracticog5/controller/LoginController.java
+++ CasoPractico-G5/src/main/java/org/ni/edu/uam/casopracticog5/controller/LoginController.java
@@ -1,11 +1,15 @@
 package org.ni.edu.uam.casopracticog5.controller;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 
 public class LoginController {
