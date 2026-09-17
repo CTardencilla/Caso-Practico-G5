4b55a810fbc304197b1650e1131df27c97a1f81f
g-nzaan <asgmshift@gmail.com>
2026-09-16 18:03:19 -0600

    Correcciones e importación de librerías a utilizar en el controlador del login

--- CasoPractico-G5/src/main/java/org/ni/edu/uam/casopracticog5/controller/LoginController.java
+++ CasoPractico-G5/src/main/java/org/ni/edu/uam/casopracticog5/controller/LoginController.java
@@ -1,14 +1,17 @@
 package org.ni.edu.uam.casopracticog5.controller;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * Controlador de LoginView.fxml (Integrante 1).
- */
+
 public class LoginController {
 
     @FXML
     public void initialize() {
-        // TODO: Inicialización del controlador de Login
+
     }
 }
